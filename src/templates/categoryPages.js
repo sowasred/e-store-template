a5998d72d482955d1d86a267db46901861814a90
sowasred <muldurozan@gmail.com>
2020-04-12 16:48:49 -0400

    category also done

--- src/templates/categoryPages.js
+++ src/templates/categoryPages.js
@@ -3,13 +3,61 @@ import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 
-const categoryPages = ({ data }) => {
+export const query = graphql`
+  query($slug: String!) {
+    contentfulCategory(slug: { eq: $slug }) {
+      title {
+        title
+      }
+      categoryDescription {
+        childMarkdownRemark {
+          html
+        }
+      }
+      headerImage {
+        fluid {
+          src
+        }
+      }
+      icon {
+        fluid {
+          src
+        }
+      }
+      slug
+      products {
+        slug
+        sku
+        id
+        discountedPrice
+        image {
+          fluid {
+            src
+          }
+        }
+        productName {
+          productName
+        }
+        quantity
+      }
+    }
+  }
+`
+
+const categoryPages = props => {
+  function createMarkup() {
+    return {
+      __html:
+        props.data.contentfulCategory.categoryDescription.childMarkdownRemark
+          .html,
+    }
+  }
+
   return (
-    <React.Fragment>
-      <Layout>
-        <h1>Derry Store</h1>
-      </Layout>
-    </React.Fragment>
+    <Layout>
+      <h1>{props.data.contentfulCategory.title.title}</h1>
+      <p dangerouslySetInnerHTML={createMarkup()} />
+    </Layout>
   )
 }
 
