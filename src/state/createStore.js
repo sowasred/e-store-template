853f15e31033701b9945815ea28747d97c86519f
sowasred <muldurozan@gmail.com>
2020-04-24 15:46:47 -0400

    before filter done

--- src/state/createStore.js
+++ src/state/createStore.js
@@ -1,9 +1,15 @@
-import { createStore, combineReducers, compose } from "redux"
+import { createStore, applyMiddleware, combineReducers, compose } from "redux"
 import { composeWithDevTools } from "redux-devtools-extension"
+import thunk from "redux-thunk"
 
 import reducer from "./reducers/index"
 
 // preloadedState will be passed in by the plugin
 export default preloadedState => {
-  return createStore(reducer, preloadedState, composeWithDevTools())
+  return createStore(
+    reducer,
+    applyMiddleware(thunk),
+    preloadedState,
+    composeWithDevTools()
+  )
 }
