125a5d9bc82fb2e15b8de9d87852acd2ad655254
sowasred <muldurozan@gmail.com>
2020-04-28 19:07:42 -0400

    place is changed

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,55 +9,6 @@ import BelowSection from "../components/belowsection"
 import MailSignup from "../components/mailsignup"
 import FeaturedProduct from "../components/featuredproduct"
 
-const IndexPage = ({ data }) => {
-  // let imageUrl = `https:${data.allContentfulMainPage.nodes[0].mainImage.fluid.src}`
-
-  let womenImage = `https:${data.allContentfulMainPage.nodes[0].firstRow[0].fluid.src}`
-
-  let menImage = `https:${data.allContentfulMainPage.nodes[0].firstRow[1].fluid.src}`
-  // let bannerImage = `https:${data.allContentfulMainPage.nodes[0].firstRow[2].fluid.src}`
-
-  let firstImage = `https:${data.allContentfulMainPage.nodes[0].secondRow[0].fluid.src}`
-  let secondImage = `https:${data.allContentfulMainPage.nodes[0].secondRow[1].fluid.src}`
-  let description =
-    data.allContentfulMainPage.nodes[0].description.content[0].content[0].value
-
-  let longDescription =
-    data.allContentfulMainPage.nodes[0].longDescription.longDescription
-
-  let featuredProducts = data.allContentfulMainPage.nodes[0].product
-  let seoTemp = data.allContentfulMainPage.nodes[0].seo
-
-  console.info("ses55", featuredProducts[0])
-
-  return (
-    <Layout>
-      <SEO title="Home" seo={seoTemp} />
-      <MainpageIntro
-        womenImage={womenImage}
-        womeImageTitle={data.allContentfulMainPage.nodes[0].firstRow[0].title}
-        menImage={menImage}
-        menImageTitle={data.allContentfulMainPage.nodes[0].firstRow[1].title}
-      />
-      <FeaturedProduct featuredProducts={featuredProducts} />
-      <BelowSection
-        firstImage={firstImage}
-        firstImageTitle={data.allContentfulMainPage.nodes[0].secondRow[0].title}
-        secondImage={secondImage}
-        secondImageTitle={
-          data.allContentfulMainPage.nodes[0].secondRow[1].title
-        }
-        longDescription={longDescription}
-      />
-
-      <MailSignup description={description} />
-
-      {/* <h1>{data.allContentfulMainPage.nodes[0].mainTitle}</h1> */}
-      {/* <img src={imageUrl} alt="Girl in a Leather Jacket" /> */}
-    </Layout>
-  )
-}
-
 export const query = graphql`
   query HomePageQuery {
     allContentfulMainPage {
@@ -118,5 +69,54 @@ export const query = graphql`
     }
   }
 `
+
+const IndexPage = ({ data }) => {
+  // let imageUrl = `https:${data.allContentfulMainPage.nodes[0].mainImage.fluid.src}`
+
+  let womenImage = `https:${data.allContentfulMainPage.nodes[0].firstRow[0].fluid.src}`
+
+  let menImage = `https:${data.allContentfulMainPage.nodes[0].firstRow[1].fluid.src}`
+  // let bannerImage = `https:${data.allContentfulMainPage.nodes[0].firstRow[2].fluid.src}`
+
+  let firstImage = `https:${data.allContentfulMainPage.nodes[0].secondRow[0].fluid.src}`
+  let secondImage = `https:${data.allContentfulMainPage.nodes[0].secondRow[1].fluid.src}`
+  let description =
+    data.allContentfulMainPage.nodes[0].description.content[0].content[0].value
+
+  let longDescription =
+    data.allContentfulMainPage.nodes[0].longDescription.longDescription
+
+  let featuredProducts = data.allContentfulMainPage.nodes[0].product
+  let seoTemp = data.allContentfulMainPage.nodes[0].seo
+
+  console.info("ses55", featuredProducts[0])
+
+  return (
+    <Layout>
+      <SEO title="Home" seo={seoTemp} />
+      <MainpageIntro
+        womenImage={womenImage}
+        womeImageTitle={data.allContentfulMainPage.nodes[0].firstRow[0].title}
+        menImage={menImage}
+        menImageTitle={data.allContentfulMainPage.nodes[0].firstRow[1].title}
+      />
+      <FeaturedProduct featuredProducts={featuredProducts} />
+      <BelowSection
+        firstImage={firstImage}
+        firstImageTitle={data.allContentfulMainPage.nodes[0].secondRow[0].title}
+        secondImage={secondImage}
+        secondImageTitle={
+          data.allContentfulMainPage.nodes[0].secondRow[1].title
+        }
+        longDescription={longDescription}
+      />
+
+      <MailSignup description={description} />
+
+      {/* <h1>{data.allContentfulMainPage.nodes[0].mainTitle}</h1> */}
+      {/* <img src={imageUrl} alt="Girl in a Leather Jacket" /> */}
+    </Layout>
+  )
+}
 
 export default IndexPage
