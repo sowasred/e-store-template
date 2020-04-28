1d98eb28280b0a3383499c2ff5b8023fa26d1cb3
sowasred <muldurozan@gmail.com>
2020-04-28 17:40:16 -0400

    gatsby env removed

--- gatsby-config.js
+++ gatsby-config.js
@@ -49,7 +49,6 @@ module.exports = {
     },
     `gatsby-plugin-stripe`,
     `gatsby-plugin-sass`,
-    `gatsby-env-variables`,
     `gatsby-plugin-react-helmet`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
