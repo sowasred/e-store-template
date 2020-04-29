7965bada32dea1b99793684797c8b7b6f789a51a
sowasred <muldurozan@gmail.com>
2020-04-28 23:29:40 -0400

    changed menu

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -47,7 +47,7 @@ const TEST_QUERY = gql`
 
 const Navigation = props => {
   const { loading, error, data } = useQuery(TEST_QUERY)
-  // console.info(loading, error, data, "loading, error, data")
+  console.info(loading, error, data, "loading, error, data")
   let windowInnerWidth = typeof window !== `undefined` ? window.innerWidth : 360
   const [currentScreenWidth, setCurrentScreenWidth] = React.useState(
     windowInnerWidth
@@ -67,22 +67,24 @@ const Navigation = props => {
 
   let tempArray = []
   const fillMenu = () => {
-    let categories = data.allContentfulNavMenu.edges[0].node.categories
-    let otherPages = data.allContentfulNavMenu.edges[0].node.otherPages
+    if (!loading) {
+      let categories = data.allContentfulNavMenu.edges[0].node.categories
+      let otherPages = data.allContentfulNavMenu.edges[0].node.otherPages
 
-    categories.map(item => {
-      tempArray.push({
-        title: item.title.title,
-        slug: item.slug,
+      categories.map(item => {
+        tempArray.push({
+          title: item.title.title,
+          slug: item.slug,
+        })
       })
-    })
-    otherPages.map(item => {
-      tempArray.push({
-        title: item.title,
-        slug: item.slug,
+      otherPages.map(item => {
+        tempArray.push({
+          title: item.title,
+          slug: item.slug,
+        })
       })
-    })
-    dispatch(fillAllMenuTitles([...tempArray]))
+      dispatch(fillAllMenuTitles([...tempArray]))
+    }
   }
   if (typeof window !== `undefined`) {
     window.onscroll = function() {
@@ -106,10 +108,10 @@ const Navigation = props => {
   }
 
   useEffect(() => {
-    if (loading) {
+    if (!loading) {
       fillMenu()
     }
-  }, [])
+  }, [loading])
 
   let isMobile
   const mobileSize = 768
