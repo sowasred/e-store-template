ff5e7c0baba77ea3aa2ed3f64a23cae0a99c7a64
sowasred <muldurozan@gmail.com>
2020-04-28 20:11:48 -0400

    check seo data

--- src/components/seo.js
+++ src/components/seo.js
@@ -22,6 +22,7 @@ function SEO({ description, metakeys, title, canonical }) {
       }
     `
   )
+  console.info("description", description, "metakeys", metakeys)
 
   // const metaDescription = seo.description.content[0].content[0].value
 
