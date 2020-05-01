22610df39016195893fcedf7a029e507c674ac3b
sowasred <muldurozan@gmail.com>
2020-04-30 21:34:23 -0400

    apollo server changed

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,6 +41,7 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-graphql-loader`,
     {
       resolve: `gatsby-transformer-remark`,
       options: {
